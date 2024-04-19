e3519c773d4ce41f55f18c2ea40fb145634a5bef
Elisha Thompson <elishathompson62@gmail.com>
2024-04-19 12:42:05 +0000

    up

--- public/data.js
+++ public/data.js
@@ -1,8 +1,16 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
-import { getFirestore, collection, getDoc, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
-import firebaseConfig from "./firebaseConfig.js";
-
-
+let config = {
+    apiKey: "AIzaSyBd5zji0Kd04frEvXYWR9dvIiMozMCC6q4",
+    authDomain: "triviatime-1c31d.firebaseapp.com",
+    projectId: "triviatime-1c31d",
+    storageBucket: "triviatime-1c31d.appspot.com",
+    messagingSenderId: "385717513256",
+    appId: "1:385717513256:web:b62e51b5f160a11050191d",
+    measurementId: "G-33GMW5QP64"
+};
+firebase.initializeApp(config);
+const db = firebase.firestore();
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
+db.collection('Scores').get().then((snapshot)=>{
+    console.log(snapshot.docs);
+})
