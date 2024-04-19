020738c20e2c1dfd1b09b5731e12bbbd96e12e80
Elisha Thompson <elishathompson62@gmail.com>
2024-04-19 13:34:55 +0000

    up

--- public/data.js
+++ public/data.js
@@ -1,16 +1,16 @@
-let config = {
-    apiKey: "AIzaSyBd5zji0Kd04frEvXYWR9dvIiMozMCC6q4",
-    authDomain: "triviatime-1c31d.firebaseapp.com",
-    projectId: "triviatime-1c31d",
-    storageBucket: "triviatime-1c31d.appspot.com",
-    messagingSenderId: "385717513256",
-    appId: "1:385717513256:web:b62e51b5f160a11050191d",
-    measurementId: "G-33GMW5QP64"
-};
-firebase.initializeApp(config);
-const db = firebase.firestore();
-const app = initializeApp(firebaseConfig);
+import{initializeApp,getApps}from 'firebase/app';
+import{getFirestore,getDocs}from 'firebase/firestore';
+import{config} from './firebaseConfig';
 
-db.collection('Scores').get().then((snapshot)=>{
-    console.log(snapshot.docs);
+initializeApp(config)
+
+const db=getFirestore()
+
+const colRef =collection(db,'Scores')
+
+//get scores
+
+getDocs(colRef)
+.then((snapshot)=>{
+    console.log(snapshot.docs)
 })
--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,46 +1,11 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
-import { getFirestore, collection, getDoc, addDoc, deleteDoc, doc, getDocs } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
-import firebaseConfig from "./firebaseConfig.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyBd5zji0Kd04frEvXYWR9dvIiMozMCC6q4",
+    authDomain: "triviatime-1c31d.firebaseapp.com",
+    projectId: "triviatime-1c31d",
+    storageBucket: "triviatime-1c31d.appspot.com",
+    messagingSenderId: "385717513256",
+    appId: "1:385717513256:web:b62e51b5f160a11050191d",
+    measurementId: "G-33GMW5QP64"
+  };
 
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-
-async function getReviews(isbn, renderFun){
-    const reviewsRef = collection(db, 'Scores');
-    const querySnapshot = await getDocs(reviewsRef);
-    const reviews = [];
-    querySnapshot.forEach((doc) => {
-        if (doc.data().isbn === isbn) {
-            reviews.push(doc.data());
-        }
-    });
-    renderFun(reviews);
-}
-
-async function createReview(auth, isbn, text){
-    const reviewData = {
-        isbn,
-        text,
-        userId: auth.currentUser.uid,
-        reviewer: auth.displayName + "z" 
-    };
-    await addDoc(collection(db, "reviews"), reviewData);
-}
-
-async function deleteReview(auth, reviewId){
-    const reviewRef = doc(db, 'reviews', reviewId);
-    const reviewDoc = await getDoc(reviewRef);
-    
-    if (reviewDoc.exists() && reviewDoc.data().userId === auth.currentUser.uid) {
-        await deleteDoc(reviewRef);
-        console.log("Review deleted successfully");
-        return true;
-    } else {
-        console.log("Review not found or you don't have permission to delete it");
-        return false;
-    }
-}
-
-export { getReviews, createReview, deleteReview };
+  export default firebaseConfig;
